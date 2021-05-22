b40a0e09cc6e44405d6d2a579457a641ed705b6d
Dmitry Bychkov <dimac090501@hotmail.com>
2021-05-22 14:43:01 +0300

    Multitype notify

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var WorkersController = require("./controllers/workersController")
 var workersController = new WorkersController();
 var server = new SocketServer({port: 3257});
 
-setInterval(() => server.send(workersController.workers), 1000);
+setInterval(() => server.send({workers: workersController.workers}), 1000);
 
 for (let {type, position} of require("./data/workers.json"))
     workersController.addWorker(type, position);
